f720b43536b56cf2a62115962bf2265dffb6c05b
Lemanyaqubova <109238717+Lemanyaqubova@users.noreply.github.com>
2022-08-02 18:19:15 +0400

    Add files via upload
    hometasks

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hometask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = "Fevral";
+            switch (month)
+            {
+                case "Fevral":
+                    Console.WriteLine("28");
+                    break;
+                case "Yanvar":
+                case "Mart":
+                case "May":
+                case "Iyul":
+                case "Avqust":
+                case "Oktyabr":
+                case "Dekabr":
+                    Console.WriteLine("31");
+                    break;
+                case "Sentyabr":
+                case "Noyabr":
+                case "Aprel":
+                case "Iyun":
+                    Console.WriteLine("30");
+                    break;
+
+            }
+        }
+    }
+}
